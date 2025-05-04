1da6d09e1aafb8c3ff6a47a74b2e98e8970461e7
MarioSilva93 <msilvaconego@gmail.com>
2025-05-04 11:30:37 +0200

    .....

--- js/empresasPorPais.js
+++
@@ -1,8 +0,0 @@
-const empresasPorPais = {
-    "Suíça": ["DPD", "Planzer", "Galliker"],
-    "Alemanha": ["Dachser", "DB Schenker", "Hermes"],
-    "França": ["La Poste", "Chronopost", "Geodis"],
-    "Espanha": ["SEUR", "Correos Express", "MRW"],
-    "Portugal": ["CTT Expresso", "Nacex", "Torrestir"]
-  };
-  
